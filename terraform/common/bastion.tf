fa98a63f22a7820e8a945e733acb2050b178840e
Ian <ian@example.com>
2025-09-15 16:30:07 +0100

    fix: increase EC2 root volume size to 30GB to match snapshot requirements
    - Update bastion host root volume from 20GB to 30GB
    - Update webserver root volume from 20GB to 30GB
    - Resolves InvalidBlockDeviceMapping error when creating instances
    - Snapshot snap-08757dc99a77c032a requires minimum 30GB volume size

--- terraform/common/bastion.tf
+++ terraform/common/bastion.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "bastion" {
   # Root block device with more storage
   root_block_device {
     volume_type           = "gp3"
-    volume_size           = 20
+    volume_size           = 30
     delete_on_termination = true
     encrypted             = true
   }
