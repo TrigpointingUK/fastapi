1dd3148a1903eca427b3960552ba953669e30fea
Ian <ian@example.com>
2025-10-08 09:25:05 +0100

    additional fields

---
+++ forum/ext/teasel/auth0/language/en/common.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Auth0 OAuth Service
+ */
+
+if (!defined('IN_PHPBB'))
+{
+    exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+    $lang = array();
+}
+
+$lang = array_merge($lang, array(
+    'AUTH0_OAUTH_SERVICE' => 'Auth0 OAuth Service',
+    'AUTH0_OAUTH_SERVICE_DESCRIPTION' => 'Provides Auth0 OAuth authentication for phpBB',
+));
+
