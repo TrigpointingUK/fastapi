818342aa60128703c5d868d2d0f8de6713ae7766
Ian <ian@example.com>
2025-10-08 10:35:55 +0100

    forum auth0

--- forum/ext/teasel/auth0/service/provider.php
+++ forum/ext/teasel/auth0/service/provider.php
@@ -13,14 +13,17 @@ use phpbb\config\config;
  */
 class provider extends base
 {
+    protected $client_id;
+    protected $client_secret;
+
     public function __construct(config $config)
     {
         $domain = getenv('AUTH0_DOMAIN') ?: '';
-        $client_id = getenv('AUTH0_CLIENT_ID') ?: '';
-        $client_secret = getenv('AUTH0_CLIENT_SECRET') ?: '';
+        $this->client_id = getenv('AUTH0_CLIENT_ID') ?: '';
+        $this->client_secret = getenv('AUTH0_CLIENT_SECRET') ?: '';
         $scope = getenv('AUTH0_SCOPE') ?: 'openid email profile';
 
-        $credentials = ['key' => $client_id, 'secret' => $client_secret];
+        $credentials = ['key' => $this->client_id, 'secret' => $this->client_secret];
         $options     = ['scope' => $scope];
         $endpoints   = [
             'base_url'     => 'https://' . $domain,
@@ -30,5 +33,32 @@ class provider extends base
         ];
 
         parent::__construct($config, $credentials, $options, $endpoints, 'auth0');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get_service_credentials()
+    {
+        return [
+            'key'    => $this->client_id,
+            'secret' => $this->client_secret,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function perform_auth_login()
+    {
+        return parent::perform_auth_login();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function perform_token_auth()
+    {
+        return parent::perform_token_auth();
     }
 }
