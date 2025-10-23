1b538f45593961fa018f18a4f14027a37f2a3f9a
Ian <ian@example.com>
2025-10-23 13:58:43 +0100

    Optimise CloudWatch costs by disabling memory autoscaling and adding CPU cooldowns
    - Comment out memory autoscaling policies in all ECS service modules
    - Add cooldown periods to CPU autoscaling policies (300s scale-in, 60s scale-out)
    - Comment out CloudWatch Synthetics monitoring alarms
    - Reduce ECS service desired count from 2 to 1 in production
    - Apply changes to both staging and production environments
    
    This should significantly reduce CloudWatch costs by eliminating:
    - Memory autoscaling alarms (1-minute evaluation periods)
    - CloudWatch Synthetics alarms
    - Reducing overall monitoring overhead

--- terraform/modules/mediawiki/main.tf
+++ terraform/modules/mediawiki/main.tf
@@ -214,20 +214,20 @@ resource "aws_appautoscaling_policy" "mediawiki_cpu" {
   }
 }
 
-# Auto Scaling Policy - Memory
-resource "aws_appautoscaling_policy" "mediawiki_memory" {
-  name               = "${var.project_name}-mediawiki-${var.environment}-memory-scaling"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.mediawiki.resource_id
-  scalable_dimension = aws_appautoscaling_target.mediawiki.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.mediawiki.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-    }
-    target_value       = var.memory_target_value
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 60
-  }
-}
+# Auto Scaling Policy - Memory (commented out to reduce CloudWatch costs)
+# resource "aws_appautoscaling_policy" "mediawiki_memory" {
+#   name               = "${var.project_name}-mediawiki-${var.environment}-memory-scaling"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.mediawiki.resource_id
+#   scalable_dimension = aws_appautoscaling_target.mediawiki.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.mediawiki.service_namespace
+#
+#   target_tracking_scaling_policy_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+#     }
+#     target_value       = var.memory_target_value
+#     scale_in_cooldown  = 300
+#     scale_out_cooldown = 60
+#   }
+# }
--- terraform/modules/phpmyadmin/main.tf
+++ terraform/modules/phpmyadmin/main.tf
@@ -123,20 +123,20 @@ resource "aws_appautoscaling_policy" "phpmyadmin_cpu" {
   }
 }
 
-# Auto Scaling Policy - Memory
-resource "aws_appautoscaling_policy" "phpmyadmin_memory" {
-  name               = "${var.project_name}-phpmyadmin-${var.environment}-memory-scaling"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.phpmyadmin.resource_id
-  scalable_dimension = aws_appautoscaling_target.phpmyadmin.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.phpmyadmin.service_namespace
-
-  target_tracking_scaling_policy_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-    }
-    target_value       = var.memory_target_value
-    scale_in_cooldown  = 300
-    scale_out_cooldown = 60
-  }
-}
+# Auto Scaling Policy - Memory (commented out to reduce CloudWatch costs)
+# resource "aws_appautoscaling_policy" "phpmyadmin_memory" {
+#   name               = "${var.project_name}-phpmyadmin-${var.environment}-memory-scaling"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.phpmyadmin.resource_id
+#   scalable_dimension = aws_appautoscaling_target.phpmyadmin.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.phpmyadmin.service_namespace
+#
+#   target_tracking_scaling_policy_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+#     }
+#     target_value       = var.memory_target_value
+#     scale_in_cooldown  = 300
+#     scale_out_cooldown = 60
+#   }
+# }
